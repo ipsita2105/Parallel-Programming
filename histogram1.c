940d65daa2842e8d581575ffca658d05736781c3
ipsita@2105 <ipsita2105@gmail.com>
2019-01-27 21:19:17 +0530

    added time

--- histogram1.c
+++ histogram1.c
@@ -5,14 +5,15 @@
 #include<unistd.h>
 #include<time.h>
 
-long long int INPUT_SIZE = 10;
-#define NUM_BINS  5
-#define NUM_THREADS  5
-float LBOUND = 0.0;
-float UBOUND =  1.0;
-float input[10];
-long long int Hfinal[5];
-long long int Htemp[NUM_THREADS][5];
+#define INPUT_SIZE 100000000
+#define NUM_BINS 100
+#define NUM_THREADS 10
+#define LBOUND 0.0
+#define UBOUND 10.0
+float input[INPUT_SIZE];
+long long int Hfinal[NUM_BINS];
+long long int Htemp[NUM_THREADS][NUM_BINS];
+
 
 void generate_input(){
 
@@ -21,8 +22,6 @@ void generate_input(){
     for(long long int i=0; i<INPUT_SIZE; i++){
         input[i] = ((float)rand()/(float)(RAND_MAX))*(UBOUND);
     }
-
-
 }
 
 int get_bin(float num){
@@ -36,11 +35,9 @@ void *calc_hist(void *tnum){
     long long int my_start = (thread_num)*(INPUT_SIZE/NUM_THREADS);
     long long int my_end   = (thread_num + 1)*(INPUT_SIZE/NUM_THREADS);
     int bin_num;
-    printf("thread num = %d my_start = %lld my_end = %lld\n", thread_num, my_start, my_end);
 
     for(long long int i= my_start; i< my_end; i++){
         bin_num = get_bin(input[i]);
-        printf("input = %f bin = %d\n",input[i],bin_num);
         Htemp[thread_num][bin_num] += 1;
     }
 
@@ -60,11 +57,13 @@ void calc_final_histogram(){
 
 int main(){
 
+    clock_t begin = clock();
+
     generate_input();
-    printf("Input is\n");
+    /*printf("Input is\n");
     for(long long int i=0; i<INPUT_SIZE; i++){
         printf("in%lli = %f\n",i,input[i]);
-    }
+    }*/
 
     pthread_t h_threads[NUM_THREADS];
     for(int t=0; t<NUM_THREADS; t++){
@@ -83,5 +82,8 @@ int main(){
     for(int b=0 ; b<NUM_BINS; b++){
         printf("B:%i = %lld\n",b, Hfinal[b]);
     }
+
+    clock_t end = clock();
+    printf("Time = %f s\n",(double)(end-begin)/CLOCKS_PER_SEC);
 
 }
