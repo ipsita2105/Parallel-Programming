8440c7d3069cf715e450eddf4c871f090caddeac
ipsita@2105 <ipsita2105@gmail.com>
2019-03-03 16:45:08 +0530

    till step 38

--- text_editor/text_editor.c
+++ text_editor/text_editor.c
@@ -6,6 +6,7 @@
 #include<ctype.h>
 #include<errno.h>
 #include<sys/ioctl.h>
+#include<string.h>
 /*****************************defines**************************/
 
 #define CTRL_KEY(k) ((k) & 0x1f) //for mapping ctrl+_ combos
@@ -151,14 +152,46 @@ int getWindowSize(int *rows, int *cols){
 	}
 }
 
+/*****************************append buffer**************************/
+
+struct abuf{
+ 
+	char *b;  //pointer to buffer
+	int len;  //len of buffer
+};
+
+#define ABUF_INIT {NULL, 0} //consttuctor empty buffer
+
+void abAppend(struct abuf *ab, const char *s, int len){
+	
+	char *new = realloc(ab->b, ab->len + len);
+
+	if(new == NULL) return;
+
+	memcpy(&new[ab->len], s, len);
+	ab->b = new;
+	ab->len += len;
+
+}
+
+void abFree(struct abuf *ab){
+	free(ab->b);
+}
+
 
 /*****************************output**************************/
 
-void editorDrawRows(){
+void editorDrawRows(struct abuf *ab){
 	
 	int y;
 	for(y=0; y<E.screenrows; y++){
-		write(STDOUT_FILENO, "~\r\n", 3);
+	
+		abAppend(ab, "~", 1);
+
+		if(y < E.screenrows - 1){
+		   abAppend(ab, "\r\n", 2);
+		}
+	
 	}
 
 }
@@ -166,12 +199,17 @@ void editorDrawRows(){
 
 void editorRefreshScreen(){
 
-	write(STDOUT_FILENO, "\x1b[2J", 4); //clear screen
-	write(STDOUT_FILENO, "\x1b[H", 3);  //reposition cursor	
+	struct abuf ab = ABUF_INIT;
 
-	editorDrawRows(); //draw ~
+	abAppend(&ab, "\x1b[2J", 4); //clear screen
+	abAppend(&ab, "\x1b[H", 3);  //reposition cursor	
 
-	write(STDOUT_FILENO, "\x1b[H", 3); //after drawing reposition cursor
+	editorDrawRows(&ab); //draw ~
+
+	abAppend(&ab, "\x1b[H", 3); //after drawing reposition cursor
+
+	write(STDOUT_FILENO, ab.b, ab.len);
+	abFree(&ab);
 }
 
 
