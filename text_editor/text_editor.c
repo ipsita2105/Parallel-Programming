feabe81bd78aa8f41a5c5c422140aa6f6e896624
ipsita@2105 <ipsita2105@gmail.com>
2019-03-03 16:31:03 +0530

    Till step 34

--- text_editor/text_editor.c
+++ text_editor/text_editor.c
@@ -104,12 +104,45 @@ char editorReadKey(){
 	return c;
 }
 
+int getCursorPosition(int *rows, int *cols){
+
+	char buf[32];
+	unsigned int i = 0;
+
+	if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+	//parse the cursor position
+	while(i < sizeof(buf)-1){
+	
+		if(read(STDIN_FILENO, &buf[i], 1) != 1) break;
+		if(buf[i] == 'R') break;
+		i++;
+	}
+
+	buf[i] = '\0';
+
+	//printf("\r\n&buf[1]: '%s'\r\n",&buf[1]); prints cursor position
+	
+	if(buf[0] != '\x1b' || buf[1] != '[') return -1;
+
+	//parse the two numbers from cursor position
+	if(sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+	return -1;
+}
+
+
+
 int getWindowSize(int *rows, int *cols){
 	
 	struct winsize ws;
 
 	if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
-		return -1;
+		//move cursor to bottom right
+		//first right then bottom
+		//999 ensures a very large value
+		if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+		return getCursorPosition(rows, cols);
 	}else{
 	
 		*cols = ws.ws_col;
