98caa76d40b11a3f7773f40ea89b451b45474b74
ipsita@2105 <ipsita2105@gmail.com>
2019-03-13 00:44:48 +0530

    till step 166

--- text_editor/text_editor.c
+++ text_editor/text_editor.c
@@ -42,11 +42,13 @@ enum editorKey{
 enum editorHighlight{
 	
 	HL_NORMAL = 0,
+	HL_STRING,
 	HL_NUMBER,
 	HL_MATCH
 };
 
 #define HL_HIGHLIGHT_NUMBERS (1 << 0)
+#define HL_HIGHLIGHT_STRINGS (1 << 1)
 
 /******************************data***************************/
 
@@ -100,7 +102,7 @@ struct editorSyntax HLDB[] = {
 	{
 	  "c",			
 	  C_HL_extensions,      
-	  HL_HIGHLIGHT_NUMBERS  
+	  HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
 	},
 };
 
@@ -362,6 +364,7 @@ int editorSyntaxToColor(int hl){
 
 	switch(hl){
 		
+		case HL_STRING: return 35;		
 		case HL_NUMBER: return 31;
 		case HL_MATCH:  return 34;
 
